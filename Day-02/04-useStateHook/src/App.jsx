b345dfd0b6f5add343c8ef3515ca5fa596e80fc6
Bikash <sahb2676@gmail.com>
2025-01-28 00:51:33 +0545

    Add updating array of object

--- Day-02/04-useStateHook/src/App.jsx
+++ Day-02/04-useStateHook/src/App.jsx
@@ -2,19 +2,44 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [movie, setMovie] = useState({
-    title: "Blue Box",
-    ratings: 10,
-  });
+  const [movies, setMovies] = useState([
+    {
+      id: 1,
+      title: "Blue Box",
+      rating: 10,
+    },
+    {
+      id: 2,
+      title: "Solo Leveling",
+      rating: 9,
+    },
+    {
+      id: 3,
+      title: "Kimi No Nawa", //this randomly popped in my mind.
+      rating: 7,
+    },
+  ]);
 
-  const handleChangeRating = (rating) =>
-    setMovie({ ...movie, ratings: rating });
+  const changeName = () =>
+    setMovies(
+      movies.map((movie) =>
+        movie.title === "Kimi No Nawa" ? { ...movie, title: "Kimi" } : movie
+      )
+    );
+
+  console.log(movies[2]);
 
   return (
     <>
-      <h1>Title : {movie.title}</h1>
-      <p>Ratings : {movie.ratings}</p>
-      <button onClick={() => handleChangeRating(100)}>Change Rating</button>
+      <ul>
+        {movies.map((movie) => (
+          <li key={Math.random()}>
+            <h1>Title : {movie.title}</h1>
+            <p>Rating : {movie.rating}</p>
+          </li>
+        ))}
+        <button onClick={changeName}>Change Name</button>
+      </ul>
     </>
   );
 }
