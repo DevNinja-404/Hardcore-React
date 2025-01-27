00f1dfcc16e3a17f2186bb06ccc6236995f4949b
Bikash <sahb2676@gmail.com>
2025-01-28 00:36:36 +0545

    Add updating the object

--- Day-02/04-useStateHook/src/App.jsx
+++ Day-02/04-useStateHook/src/App.jsx
@@ -2,73 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [friends, setFriends] = useState(["Bikash", "Manish", "Rahul"]);
+  const [movie, setMovie] = useState({
+    title: "Blue Box",
+    ratings: 10,
+  });
 
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => {
-    setCount(count - 1);
-  };
-  // If we update the value of count as below without using the setCount,well the value of count does infact update but it doesn't get reflected on the UI because using setCount not only changes the state but infact also reflects or makes react known that the state has changed and the component needs to be reloaded/rerendered
-  // const increment = () => {
-  //   return count + 1;
-  // };
-  // const decrement = () => {
-  //   return count - 1;
-  // };
-
-  // updating Array:
-  const addOneFriend = (friendToAdd) => setFriends([...friends, friendToAdd]);
-  const removeOneFriend = (friendToRemove) =>
-    setFriends(friends.filter((friend) => friend !== friendToRemove));
-  const updateOneFriend = (friendToUpdate, updateTo) =>
-    setFriends(
-      friends.map((friend) => (friend === friendToUpdate ? updateTo : friend))
-    );
-
-  // updating Object:
-
-  // const
+  const handleChangeRating = (rating) =>
+    setMovie({ ...movie, ratings: rating });
 
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-
-      {/* updating Array */}
-      <div>
-        <ul>
-          {friends.map((friend, index) => (
-            <li key={index}>{friend}</li>
-          ))}
-        </ul>
-        <button onClick={() => addOneFriend("Bablu")}>Add One Friend</button>
-        <button onClick={() => removeOneFriend("Manish")}>
-          Remove One Friend
-        </button>
-        <button onClick={() => updateOneFriend("Bablu", "Bablu Prasad")}>
-          Update One Friend
-        </button>
-      </div>
-
-      {/* updating Objects */}
-      <div>
-        <ul>
-          {friends.map((friend, index) => (
-            <li key={index}>{friend}</li>
-          ))}
-        </ul>
-        <button onClick={() => addOneFriend("Bablu")}>Add One Friend</button>
-        <button onClick={() => removeOneFriend("Manish")}>
-          Remove One Friend
-        </button>
-        <button onClick={() => updateOneFriend("Bablu", "Bablu Prasad")}>
-          Update One Friend
-        </button>
-      </div>
+      <h1>Title : {movie.title}</h1>
+      <p>Ratings : {movie.ratings}</p>
+      <button onClick={() => handleChangeRating(100)}>Change Rating</button>
     </>
   );
 }
