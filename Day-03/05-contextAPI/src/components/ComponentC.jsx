ee4c86eaac76bc6b4040eb36019d6cec792b1d41
Bikash <sahb2676@gmail.com>
2025-01-30 23:09:03 +0545

    Add using the useContext hook to easily consume context

--- Day-03/05-contextAPI/src/components/ComponentC.jsx
+++ Day-03/05-contextAPI/src/components/ComponentC.jsx
@@ -1,30 +1,18 @@
-// In order to consume the value from the context API, we need to import that context in the component where we want to consume it. In this case, we want to consume the value in ComponentC, so we need to import the context in ComponentC.jsx
-
+import { useContext } from "react";
 import { Data, Data1 } from "../App";
 
 const ComponentC = () => {
-  // Consumer property on the instance of context will allow us to consume the value from the context API.It takes a callback function as a child and returns the value of the context
+  //   We need to tell the useContext hook which context we want to consume. We can do this by passing the context as an argument to the useContext hook. Here we are consuming the Data context in our ComponentC component. We can also consume multiple contexts in the same component by calling the useContext hook multiple times with different contexts as arguments.
+  const userName = useContext(Data);
+  const age = useContext(Data1);
+
+  //   Doing so will no longer require us to access or consume the context value by using the Consumer property on the instance of the context and passing a callback function as a child. Instead, we can directly access the value of the context by calling the useContext hook and passing the context as an argument to it.
+
   return (
-    <Data.Consumer>
-      {(name) => {
-        return (
-          <Data1.Consumer>
-            {(age) => {
-              return (
-                <h1>
-                  My name is {name} and age is {age}
-                </h1>
-              );
-            }}
-          </Data1.Consumer>
-        );
-      }}
-    </Data.Consumer>
+    <h1>
+      My name is {userName} and age is {age}.
+    </h1>
   );
 };
-
-// Even though we didn't drill the props from ComponentA to ComponentC, we can still access the value of name from the context API
-
-// We can provide multiple context values in the same component.Here we are provifing two context values name and age in our App component and here we are consuming both the values in ComponentC which is not a good practice but just to show you that we can consume multiple context values in the same component we did this.Even though its a good way to pass our data to the nested child but its still better than prop drilling.
 
 export default ComponentC;
