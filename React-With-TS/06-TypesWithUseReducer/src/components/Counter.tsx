d357511f451fb4314903aa59eaf837b21f449330
Bikash <sahb2676@gmail.com>
2025-02-21 14:31:00 +0545

    Add using type with useReducer

---
+++ React-With-TS/06-TypesWithUseReducer/src/components/Counter.tsx
@@ -0,0 +1,36 @@
+import { useReducer } from "react";
+
+type Action =
+  | {
+      type: "increment";
+    }
+  | { type: "decrement" };
+
+type counterState = {
+  count: number;
+};
+
+const counterReducer = (state: counterState, action: Action): counterState => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
+
+  return (
+    <div>
+      <p>Count : {state.count}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
