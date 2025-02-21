e8d1d843c3c26e3d1c19c45d466e5b3ba25c6e96
Bikash <sahb2676@gmail.com>
2025-02-21 15:42:05 +0545

    Add react with tailwind setup

---
+++ 07-React-With-Tailwind/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-slate-950 w-[100vw] min-h-[100vh] flex items-center justify-center">
+      <p className="text-6xl text-red-900">Tailwind Is Fun</p>
+    </div>
+  );
+};
+
+export default App;
